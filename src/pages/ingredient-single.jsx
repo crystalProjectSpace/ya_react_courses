0de1789a88808c78081cc2329ed82fc5277b2c7c
skuznetsov <skuznetsov@fura.ru>
2025-08-02 16:17:04 +0300

    feat: stylstic fixes

--- src/pages/ingredient-single.jsx
+++ src/pages/ingredient-single.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useParams, Link } from "react-router";
 import { SET_SELECTION } from "../services/actions";
 import { IngredientDetails } from "../components/IngredientDetails/IngredientDetails";
+import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { getItems } from "../services";
 import { API_URL } from "../constants";
 
@@ -17,7 +18,12 @@ export function IngredientSinglePage () {
         if (!hasItems) dispatch(getItems(API_URL))
     }, []);
 
-    return (<section className="page-wrap _ingredients">
-        <IngredientDetails />
+    return (<section className="page-wrap _centered _ingredients">
+        <div className="page-content">
+            <Link to="/" state={null} className="_page-icon-close">
+                <CloseIcon type="primary" />
+            </Link>
+            <IngredientDetails />
+        </div>        
     </section>)
 }
